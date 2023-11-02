9763981f1c63a9eaa2cf1cb2c4c554d80d03e03b
Vikram Reddy <84522932+vikramreddy2662@users.noreply.github.com>
2023-11-02 12:22:18 +0530

    Add files via upload

---
+++ circumference.js
@@ -0,0 +1,6 @@
+let radius=window.prompt("enter the radius of a circle");
+//const the value that cannot be changed
+const PI=3.14;
+
+let circumference=2*PI*radius;
+console.log("The circumference of a circle is:", circumference);
