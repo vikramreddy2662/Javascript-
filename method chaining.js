3a6adb42f6a9bdc3435bcaa97646e9dc6520d0c4
Vikram Reddy <84522932+vikramreddy2662@users.noreply.github.com>
2023-11-03 19:05:33 +0530

    Add files via upload

---
+++ method chaining.js
@@ -0,0 +1,6 @@
+let userName="Vikram reddy";
+let letter=userName.charAt(5).toUpperCase().trim();
+console.log(letter);
+//when we want to execute a method after another in a continuous execution then we can use method chaining.
+let my_Name=userName.slice(0,6).toUpperCase().trim();
+console.log(my_Name);
