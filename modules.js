8a0c469ddd0d95b9a425c4e93bbfee91decc02b0
Vikram Reddy <84522932+vikramreddy2662@users.noreply.github.com>
2023-11-12 19:49:56 +0530

    Add files via upload

---
+++ modules.js
@@ -0,0 +1,9 @@
+export let x;
+export let y;
+
+export function  getArea(x,y){
+return x*y;
+}
+export function getPerimeter(x,y){
+   return 2*(x+y);
+}
---
+++ modulesimport.js
@@ -0,0 +1,16 @@
+import{x,y,getArea,getPerimeter} from "./modules.js";
+
+let Area=getArea(12,23);
+let Perimeter=getPerimeter(32,45);
+
+console.log(Area);
+console.log(Perimeter);
+/* we can import functionality from one js file into another js file.  using modules*/
+
+/*import *  as MathUtil from "./modules.js";
+
+let Area=MathUtil.getArea(12,23);
+let Perimeter=MathUtil.getPerimeter(32,45);
+
+console.log(Area);
+console.log(Perimeter);*/
